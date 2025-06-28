5c26d9408273f34e9c9b325cd4dc153a11a3b058
Binity <benlo@foxmail.com>
2025-06-28 16:49:28 +0800

    -

--- __init__.py
+++ __init__.py
@@ -10,7 +10,6 @@ from .TextSwitch import TextSwitch
 from .ImgSwitch.ImageSwitch2way import ImageSwitch2way
 from .ImgSwitch.ImageSwitch4way import ImageSwitch4way
 from .ExtraOptions import ExtraOptions
-from .ColorTracking import ColorTracking
 from .PromptOptimizer import PromptOptimizer
 from .SystemPrompt.SystemPromptLoader import SystemPromptLoader
 from .ColorRemoval import ColorRemoval
@@ -29,7 +28,6 @@ NODE_CLASS_MAPPINGS = {
     "TextSwitch": TextSwitch,
     "ImageSwitch2way": ImageSwitch2way,
     "ExtraOptions": ExtraOptions,
-    "ColorTracking": ColorTracking,
     "ImageSwitch4way": ImageSwitch4way,
     "PromptOptimizer": PromptOptimizer,
     "SystemPromptLoader": SystemPromptLoader,
@@ -49,7 +47,6 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ImageSwitch2way": "图像切换器-2路(可注释)",
     "ImageSwitch4way": "图像切换器-4路(可注释)",
     "ExtraOptions": "额外引导选项（通用）",
-    "ColorTracking": "颜色跟踪器",
     "BaiduTranslateNode": "百度翻译",
     "PromptOptimizer": "提示词优化器",
     "SystemPromptLoader": "系统引导词加载器",
